f4dc259ddf05f361d1b763156b19ca3fe0324373
Ezequiel Gomez <97802917+JuanEzequielGomez@users.noreply.github.com>
2022-08-11 14:14:30 -0300

    Ejercicio 10 de Introducción a la programación

---
+++ gomez_ej10.py
@@ -0,0 +1,11 @@
+# El ejercicio elegido para esta valoración final fue el número 10:
+
+# Crear una lista denominada “Clientes” con los nombres de los diferentes dueños de perros, de la siguiente manera:
+
+#       Juan,  Mario,  Ariel,  Josefina,  Marianella.
+
+# Ordenarla alfabéticamente y mostrarla por pantalla.
+
+clientes = ['Juan', 'Mario', 'Ariel', 'Josefina', 'Marianela']
+clientes.sort()
+print(clientes)
