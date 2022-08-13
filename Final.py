b3b115a79d6ea8f2b5fadd5d411d017721e919e2
jimebert0741 <101677259+jimebert0741@users.noreply.github.com>
2022-08-12 23:24:08 -0300

    Add files via upload

---
+++ Final.py
@@ -0,0 +1,8 @@
+# Ejercicio elegido: Numero 2
+
+# Crea una lista denominada "Dueno2" y recorrerla en un bucle mostrando sus elementos por pantalla con excepcion del DNI y el apellido.
+
+# 23546987, "Maria", "Perez", 4789689, "Pueyrredon 811"
+
+Dueno2= [23546987, 'Maria', 'Perez', 4789689, 'Pueyrredon 811']
+print('Nombre:', Dueno2[1] , 'Telefono:', Dueno2[3] , 'Direccion:', Dueno2[4])
